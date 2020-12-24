32dab7e7eafaf3756e2ad96c0c79f431a60297e7
Eric T <9063787+tsairinius@users.noreply.github.com>
2020-12-24 11:03:21 -0800

    getPersonBirthYearQuestion function created

--- app.js
+++ app.js
@@ -49,19 +49,31 @@ export async function getPersonWithId(id) {
     return person;
 }
 
+export function getPersonBirthYearQuestion(person) {
+    let result = null;
+    const isArgumentValid = (
+        person && 
+        person.name && 
+        person.birth_year && 
+        person.name !== "unknown" && 
+        person.birth_year !== "unknown"
+    );
+
+    if (isArgumentValid) {
+        result = {
+            question: `What is the birth year of ${person.name}?`,
+            answer: person.birth_year
+        }
+    }
+
+    return result;
+};
+
+// fetch("https://swapi.dev/api/people/").then(response => response.json()).then(json => console.log(json))
+
 // (async () => console.log(await getTotalNumPeople().catch(e => console.log("Problem: ", e))))()
 
 // (async () => console.log(await getPersonWithId(0)))()
-
-// const createBirthyearQuestion = () => {
-
-//     return {
-//             question: `What is the birth year of ${randomPerson.name}?`,
-//             answer: randomPerson.birth_year
-//         }
-// }
-
-// module.exports = getTotalNumPeople;
 
 /*
 Questions:
--- app.test.js
+++ app.test.js
@@ -134,4 +134,58 @@ describe("getPersonWithId", () => {
         expect(person).toBe(null);
         expect(consoleErrorMock).toHaveBeenCalledTimes(1);
     });
-})
+});
+
+describe("getPersonBirthYearQuestion", () => {
+    test("Returns object with personalized question and answer", () => {
+        const expectedResult = {
+            question: "What is the birth year of Plo Koon?",
+            answer: "22BBY"
+        };
+
+        const person = {
+            name: "Plo Koon",
+            birth_year: "22BBY"
+        };
+
+        expect(app.getPersonBirthYearQuestion(person)).toEqual(expectedResult);
+    });
+
+    test("Returns null if person passed in is undefined", () => {
+        expect(app.getPersonBirthYearQuestion(undefined)).toBe(null);
+    });
+
+    test("Returns null if person passed in is missing name property", () => {
+        const person = {
+            birth_year: "22BBY"
+        };
+
+        expect(app.getPersonBirthYearQuestion(person)).toBe(null);
+    });
+
+    test("Returns null if person passed in is missing birth year property", () => {
+        const person = {
+            name: "Plo Koon"
+        };
+
+        expect(app.getPersonBirthYearQuestion(person)).toBe(null);
+    });
+
+    test("Returns null if person passed in has unknown name", () => {
+        const person = {
+            name: "unknown",
+            birth_year: "22BBY"
+        };
+
+        expect(app.getPersonBirthYearQuestion(person)).toBe(null);
+    });
+
+    test("Returns null if person passed in has unknown birth year", () => {
+        const person = {
+            name: "Plo Koon",
+            birth_year: "unknown"
+        };
+
+        expect(app.getPersonBirthYearQuestion(person)).toBe(null);
+    });
+});
