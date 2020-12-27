06fa260252999c6f11684ef28620e5a455583fbb
Eric T <9063787+tsairinius@users.noreply.github.com>
2020-12-27 10:36:34 -0800

    Created createRandomBirthYear function along with tests

--- app.js
+++ app.js
@@ -18,9 +18,43 @@ export function getPersonBirthYearQuestion(person) {
     return result;
 };
 
+export function createRandomBirthYear(refYear = "0BBY") {
+    if (typeof refYear !== "string") {
+        throw Error(`Argument refYear ${refYear} must be a string ending in BBY or ABY. Ex: '30BBY'`);
+    }
 
-// fetch("https://swapi.dev/api/planets/").then(response => response.json()).then(json => console.log(json))
+    if (refYear === "unknown") {
+        throw Error("Argument refYear is 'unknown'");
+    }
+
+    const era  = refYear.slice(-3);
+    if (era !== "BBY" && era !== "ABY") {
+        throw Error(`Argument refYear ${refYear} must end in BBY or ABY. Ex: '30BBY'`);
+    }
+
+    let refYearValue = Number(refYear.slice(0, -3));
+    refYearValue = era === "BBY" ? -refYearValue : refYearValue;
+
+    const min = refYearValue - 50;
+    const max = refYearValue + 50;
+    
+    let year = Math.random()*(max - min) + min;
+    const shouldFloor = Math.random() < 0.5;
+    year = shouldFloor ? Math.floor(year) : year.toFixed(1);
+
+    const result = year < 0 ? `${-year}BBY` : `${year}ABY`;
+
+    return result;
+};
+
+// createRandomBirthYearAround("1000BBY")
+
+
+// fetch("https://swapi.dev/api/people/").then(response => response.json()).then(json => console.log(json))
 
 // (async () => console.log(await getTotalNumPeople().catch(e => console.log("Problem: ", e))))()
 
 // (async () => console.log(await getPersonWithId(0)))()
+
+
+// max birthyear: 35ABY. min birthyear: 1000BBY
--- app.test.js
+++ app.test.js
@@ -53,3 +53,47 @@ describe("getPersonBirthYearQuestion", () => {
         expect(app.getPersonBirthYearQuestion(person)).toBe(null);
     });
 });
+
+describe("createRandomBirthYear", () => {
+    const mathRandomMock = jest.spyOn(global.Math, "random");
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("Throws error if reference year passed in is not a string", () => {
+        expect(() => app.createRandomBirthYear(25)).toThrow("Argument refYear 25 must be a string ending in BBY or ABY. Ex: '30BBY'");
+    });
+
+    test("Throws error if reference year is 'unknown'", () => {
+        expect(() => app.createRandomBirthYear("unknown")).toThrow("Argument refYear is 'unknown'");
+    });
+
+    test("Throws error if reference year does not end in BBY or ABY", () => {
+        expect(() => app.createRandomBirthYear("25")).toThrow("Argument refYear 25 must end in BBY or ABY. Ex: '30BBY'");
+    });
+
+    test("Returns year within fifty years of reference year", () => {
+        mathRandomMock
+            .mockReturnValueOnce(0.4)
+            .mockReturnValueOnce(0.6);
+
+        expect(app.createRandomBirthYear("5BBY")).toBe("15BBY");
+    });
+
+    test("Returns year to one decimal place based on randomly generated numbers", () => {
+        mathRandomMock
+            .mockReturnValueOnce(0.1357)
+            .mockReturnValueOnce(0.6);
+
+        expect(app.createRandomBirthYear("20ABY")).toBe("16.4BBY");
+    });
+
+    test("Returns year as integer based on randomly generated numbers", () => {
+        mathRandomMock
+            .mockReturnValueOnce(0.1357)
+            .mockReturnValueOnce(0.1);
+
+        expect(app.createRandomBirthYear("20ABY")).toBe("17BBY");
+    });
+});
