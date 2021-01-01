f015ff0365db596d953e8295ff727cf359e24c33
Eric T <9063787+tsairinius@users.noreply.github.com>
2020-12-31 16:29:35 -0800

    Created unit tests for displayNewQuestion

--- app.js
+++ app.js
@@ -4,11 +4,62 @@ import * as birthYear from "./birthYear.js";
 // const queue = new QuestionQueue();
 
 export function displayNewQuestion(question) {
-    const element = document.querySelector('p');
-    if (!element.textContent) {
-        element.textContent = question.question;
+    const isInvalidQuestion = (
+        !question || 
+        !question.question || 
+        !question.answer || 
+        (question.answer === "unknown") ||
+        question.otherOptions.length !== 3 ||
+        question.otherOptions.some(option => (!option || option === "unknown" || typeof option !== "string"))
+    );
+
+    const questionElement = document.querySelector(".question");
+    if (isInvalidQuestion) {
+        throw new Error(`Invalid question passed in. Must have valid question, answer, and three other choices: ${question ? JSON.stringify(question) : question}`);
+    }
+    
+    try {
+        const answerChoices = [question.answer, ...question.otherOptions];
+        questionElement.textContent = question.question;
+        const answerChoiceElements = document.querySelectorAll("input[type=radio]");
+        answerChoiceElements.forEach((element, index) => {
+            element.setAttribute("value", answerChoices[index]);
+            element.labels[0].textContent = answerChoices[index];
+        });
+    }
+    catch (e) {
+        throw new Error(`Unable to display question and answer choices: ${e}`);
     }
 }
+
+export function displayQuestionStructure() {
+    const structure = document.createElement("div");
+    structure.setAttribute("class", "question-container");
+    structure.innerHTML = `
+        <p class="question"></p>
+        <input type="radio" name="answer-choice" id="answer-choice-1" />
+        <label for="answer-choice-1"></label>
+        <input type="radio" name="answer-choice" id="answer-choice-2" />
+        <label for="answer-choice-2"></label>
+        <input type="radio" name="answer-choice" id="answer-choice-3" />
+        <label for="answer-choice-3"></label>
+        <input type="radio" name="answer-choice" id="answer-choice-4" />
+        <label for="answer-choice-4"></label>
+        <button>Next</button>
+    `;
+
+    document.body.appendChild(structure);
+};
+
+displayQuestionStructure();
+const question = {
+    question: "What day is it?",
+    answer: "Monday",
+    otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+};
+displayNewQuestion(question);
+
+
 // const question = {
 //     question: "What day is it?",
 //     answer: "Monday",
--- app.test.js
+++ app.test.js
@@ -2,20 +2,120 @@ import * as app from "./app.js";
 import { screen } from "@testing-library/dom";
 import "@testing-library/jest-dom";
 
-beforeAll(() => {
-    const element = document.createElement('p');
-    document.body.appendChild(element);
-})
+function cleanUpDOM() {
+    document.body.innerHTML = '';
+}
 
+describe("displayQuestionStructure", () => {
+    beforeEach(cleanUpDOM);
 
-test("Displays question", () => {
+    test("Displays paragraph element, four inputs and labels, and next button", () => {
+        app.displayQuestionStructure();
+
+        expect(document.querySelector("p")).toBeInTheDocument();
+        expect(document.querySelectorAll("input").length).toBe(4);
+        expect(screen.getByRole("button", {name: "Next"})).toBeInTheDocument();
+        expect(document.querySelectorAll("label").length).toBe(4);
+    });
+});
+
+describe("displayNewQuestion", () => {
     const question = {
         question: "What day is it?",
         answer: "Monday",
         otherOptions: ["Tuesday", "Wednesday", "Thursday"]
     };
 
-    app.displayNewQuestion(question)
+    beforeEach(() => {
+        cleanUpDOM();
+    });
 
-    expect(screen.getByText("What day is it?")).toBeInTheDocument();
+    test("Displays question with answer choices", () => {
+        app.displayQuestionStructure();
+        app.displayNewQuestion(question);
+
+        expect(screen.getByText("What day is it?")).toBeInTheDocument();
+        expect(screen.getByText("Monday")).toBeInTheDocument();
+        expect(screen.getByText("Tuesday")).toBeInTheDocument();
+        expect(screen.getByText("Wednesday")).toBeInTheDocument();
+        expect(screen.getByText("Thursday")).toBeInTheDocument();
+    });
+
+    test("If unable to find question element to insert new question, throw error", () => {
+        expect(() => app.displayNewQuestion(question))
+            .toThrow("Unable to display question and answer choices");
+    });
+
+    test("Throws error if argument is undefined", () => {
+        expect(() => app.displayNewQuestion())
+        .toThrow("Invalid question passed in. Must have valid question, answer, and three other choices");    
+    });
+
+    test("Throws error if question property of argument is undefined", () => {
+        const badQuestion = {
+            question: undefined,
+            answer: "Monday",
+            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+        };
+
+        expect(() => app.displayNewQuestion(badQuestion))
+            .toThrow("Invalid question passed in. Must have valid question, answer, and three other choices");
+    });
+
+    test("Throws error if answer property of argument is 'unknown'", () => {
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "unknown",
+            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+        };
+
+        expect(() => app.displayNewQuestion(badQuestion))
+            .toThrow("Invalid question passed in. Must have valid question, answer, and three other choices");
+    });
+
+    test("Throws error if answer property of argument is undefined", () => {
+        const badQuestion = {
+            question: "What day is it?",
+            answer: undefined,
+            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+        };
+
+        expect(() => app.displayNewQuestion(badQuestion))
+            .toThrow("Invalid question passed in. Must have valid question, answer, and three other choices");
+    });
+
+    test("Throws error if there are not three other answer choices in argument", () => {
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "Monday",
+            otherOptions: ["Wednesday", "Thursday"]
+        };
+
+        expect(() => app.displayNewQuestion(badQuestion))
+        .toThrow("Invalid question passed in. Must have valid question, answer, and three other choices");
+    });
+
+    test("Throws error if one of the other answer choices in argument is unknown", () => {
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "Monday",
+            otherOptions: ["unknown", "Wednesday", "Thursday"]
+        };
+
+        expect(() => app.displayNewQuestion(badQuestion))
+        .toThrow("Invalid question passed in. Must have valid question, answer, and three other choices");
+    });
+
+    test("Throws error if one of the other answer choices in argument is undefined", () => {
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "Monday",
+            otherOptions: [undefined, "Wednesday", "Thursday"]
+        };
+
+        expect(() => app.displayNewQuestion(badQuestion))
+        .toThrow("Invalid question passed in. Must have valid question, answer, and three other choices");
+    });
+
 });
+
