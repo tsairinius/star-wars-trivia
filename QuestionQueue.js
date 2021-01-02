be70379fe8833bb003b6818f17e7a5d5f37d92b5
Eric T <9063787+tsairinius@users.noreply.github.com>
2021-01-01 16:15:57 -0800

    Refactored question queue to have separate method for validing a question -Also added check to make sure no questions with duplicate answer choices are added to the queue

--- QuestionQueue.js
+++ QuestionQueue.js
@@ -8,35 +8,54 @@ export class QuestionQueue {
     };
 
     addQuestion(question) {
-        const isInvalidQuestion = (
-            !question || 
-            (typeof question.question !== "string") ||
-            !question.question || 
-            !question.answer || 
-            (typeof question.answer !== "string") ||
-            (question.answer === "unknown") ||
-            !question.otherOptions ||
-            question.otherOptions.length !== 3 ||
-            question.otherOptions.some(option => (!option || option === "unknown" || typeof option !== "string"))
-        );
-
-        if (isInvalidQuestion) {
-            console.error(`Denied adding invalid question to question queue: ${question ? JSON.stringify(question) : question}`);
-        }
-        else if (this.isDuplicateQuestion(question.question)) {
-            console.error("No duplicate questions allowed in queue");
-        }
-        else {
+        if (!this.shouldRejectQuestion(question)) {
             this.queue = [question, ...this.queue];  
-        }
+        };
     };
 
-    isDuplicateQuestion(question) {
-        if (!question || typeof question !== "string") {
-            throw new TypeError("The argument 'question' of type string is required");
-        }
+    shouldRejectQuestion(question) {
+        const isInvalidQuestion = () => {
+            const isInvalid = (
+                !question || 
+                (typeof question.question !== "string") ||
+                !question.question || 
+                !question.answer || 
+                (typeof question.answer !== "string") ||
+                (question.answer === "unknown") ||
+                !question.otherOptions ||
+                question.otherOptions.length !== 3 ||
+                question.otherOptions.some(option => (!option || option === "unknown" || typeof option !== "string"))
+            )
 
-        const questions = this.queue.map(item => item.question);
-        return questions.some(currQuestion => currQuestion === question);
+            if (isInvalid) {
+                console.error(`Denied adding invalid question to question queue: ${question ? JSON.stringify(question) : question}`);
+            };
+
+            return isInvalid;
+        };
+
+        const isDuplicateQuestion = () => {
+            const questions = this.queue.map(item => item.question);
+            const isDuplicate = questions.some(currQuestion => currQuestion === question.question);
+
+            if (isDuplicate) {
+                console.error("No duplicate questions allowed in queue");
+            };
+
+            return isDuplicate;
+        };
+
+        const hasDuplicateAnswerChoice = () => {
+            const answers = [question.answer, ...question.otherOptions];
+            const hasDuplicate = answers.some((answer, index) => answers.indexOf(answer) !== index);
+
+            if (hasDuplicate) {
+                console.error(`There are duplicate answer choices for question: ${JSON.stringify(question)}`);
+            };
+
+            return hasDuplicate;
+        };
+
+        return (isInvalidQuestion() || isDuplicateQuestion() || hasDuplicateAnswerChoice());
     };
 };
--- QuestionQueue.test.js
+++ QuestionQueue.test.js
@@ -47,16 +47,8 @@ describe("addQuestion", () => {
         expect(queue.queue.length).toBe(1);
         expect(queue.queue[0]).toEqual(question);
     });
-
-    test("Queue does not add undefined questions", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        queue.addQuestion(undefined);
     
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions with empty strings for question property", () => {
+    test("Check that question validation is being called. Questions with empty string for question property is invalid", () => {
         consoleErrorMock.mockReturnValueOnce();
         const badQuestion = {
             question: "",
@@ -69,162 +61,8 @@ describe("addQuestion", () => {
         expect(queue.queue.length).toBe(0);
         expect(consoleErrorMock).toHaveBeenCalledTimes(1);
     });
-    
-    test("Queue does not add questions with undefined question property", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: undefined,
-            answer: "Monday",
-            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions with question property that is not a string", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: 437,
-            answer: "Monday",
-            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions with empty strings for answer property", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: "What day is it?",
-            answer: "",
-            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions with 'unknown' as value for answer property", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: "What day is it?",
-            answer: "unknown",
-            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions with undefined answer property", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: "What day is it?",
-            answer: undefined,
-            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions with answer property that is not a string", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: "What day is it?",
-            answer: 123,
-            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions that have no other answer choices defined", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: "What day is it?",
-            answer: "Monday",
-            otherOptions: undefined
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions that do not have exactly three other choices as answers", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: "What day is it?",
-            answer: "Monday",
-            otherOptions: ["Wednesday", "Thursday"]
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions with empty string in other answer choices", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: "What day is it?",
-            answer: "Monday",
-            otherOptions: ["", "Wednesday", "Thursday"]
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions with undefined in other answer choices", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: "What day is it?",
-            answer: "Monday",
-            otherOptions: [undefined, "Wednesday", "Thursday"]
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
-    
-    test("Queue does not add questions with 'unknown' in other answer choices", () => {
-        consoleErrorMock.mockReturnValueOnce();
-        const badQuestion = {
-            question: "What day is it?",
-            answer: "Monday",
-            otherOptions: ["unknown", "Wednesday", "Thursday"]
-        }
-    
-        queue.addQuestion(badQuestion);
-    
-        expect(queue.queue.length).toBe(0);
-        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
-    });
 
-    test("Queue does not add duplicate questions", () => {
+    test("Double check question validation is being called. Queue does not add duplicate questions", () => {
         consoleErrorMock.mockReturnValueOnce();
         const duplicate = { ...question };
         queue.addQuestion(question);
@@ -235,29 +73,200 @@ describe("addQuestion", () => {
     });
 });
 
-describe("isDuplicateQuestion", () => {
+describe("shouldRejectQuestion", () => {
+    let consoleErrorMock;
+    beforeAll(() => {
+        jest.restoreAllMocks();
+        consoleErrorMock = jest.spyOn(console, "error");
+    });
+
     let queue;
     beforeEach(() => {
+        consoleErrorMock.mockClear();
         queue = new QuestionQueue();
-        queue.addQuestion(question);
+    });
+
+    test("Returns false if all answer choices are unique", () => {
+        expect(queue.shouldRejectQuestion(question)).toBe(false);
+    });
+
+    test("Returns true if a duplicate choice exists as an answer", () => {
+        consoleErrorMock.mockReturnValue();
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "Monday",
+            otherOptions: ["Tuesday", "Wednesday", "Tuesday"]
+        };
+
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
     });
 
     test("Returns false if question passed in is unique to questions in queue", () => {
-        const uniqueQuestion = "What color is the sky?";
-        expect(queue.isDuplicateQuestion(uniqueQuestion)).toBe(false);
+        queue.addQuestion(question);
+        const uniqueQuestion = {
+            question: "What color is the sky?",
+            answer: "blue",
+            otherOptions: ["yellow", "pink", "orange"]
+        };
+
+        expect(queue.shouldRejectQuestion(uniqueQuestion)).toBe(false);
     });
 
     test("Returns true if question passed in is a duplicate", () => {
-        const duplicate = question.question;
-        expect(queue.isDuplicateQuestion(duplicate)).toBe(true);
+        consoleErrorMock.mockReturnValue();
+        queue.addQuestion(question);
+        const duplicate = { ...question };
+        expect(queue.shouldRejectQuestion(duplicate)).toBe(true);
     });
 
-    test("Throws error if argument passed in is not of type string", () => {
-        expect(() => queue.isDuplicateQuestion(1)).toThrow("The argument 'question' of type string is required");
+    test("Returns true if question is undefined", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        expect(queue.shouldRejectQuestion(undefined)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
     });
-
-    test("Throws error if argument is undefined", () => {
-        expect(() => queue.isDuplicateQuestion()).toThrow("The argument 'question' of type string is required");
-    }); 
+    
+    test("Returns true if question property of argument is an empty string", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: "",
+            answer: "Monday",
+            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if question property of argument is undefined", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: undefined,
+            answer: "Monday",
+            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if question property is not a string", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: 437,
+            answer: "Monday",
+            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if answer property of argument is an empty string", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "",
+            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if answer property is 'unknown", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "unknown",
+            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if answer property is undefined", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: "What day is it?",
+            answer: undefined,
+            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if answer property is not a string", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: "What day is it?",
+            answer: 123,
+            otherOptions: ["Tuesday", "Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if array of other answer choices is undefined", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "Monday",
+            otherOptions: undefined
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if argument does not provide exactly three other answer choices", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "Monday",
+            otherOptions: ["Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if one of the other answer choices is an empty string", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "Monday",
+            otherOptions: ["", "Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if one of the other answer choices is undefined", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "Monday",
+            otherOptions: [undefined, "Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
+    
+    test("Returns true if one of the other answer choices is 'unknown'", () => {
+        consoleErrorMock.mockReturnValueOnce();
+        const badQuestion = {
+            question: "What day is it?",
+            answer: "Monday",
+            otherOptions: ["unknown", "Wednesday", "Thursday"]
+        }
+    
+        expect(queue.shouldRejectQuestion(badQuestion)).toBe(true);
+        expect(consoleErrorMock).toHaveBeenCalledTimes(1);
+    });
 });
 
