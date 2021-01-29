06f83dd6776fe41a39be3e3d745377d412afb5b4
Eric T <9063787+tsairinius@users.noreply.github.com>
2021-01-29 14:59:55 -0800

    Reorganized folder structure -Added favicons

---
+++ js/questions.js
@@ -0,0 +1,40 @@
+    // I find your lack of competence disturbing
+    // Have you been living under a rock on Dagobah? 
+    // You make those bucketheads look smart! 
+    // Well I'll be a son of a Bantha! 
+    // Well done! You've earned yourself 
+    // The force is strong with this one! 
+    // A wookie could do better!
+
+
+// Questions:
+// -From the planets api: 
+//     What planet is associated with this kind of terrain? 
+//     What film(s) did this planet appear in? 
+//     Which of the following planets is not from the Star Wars universe? 
+// -From people api:
+//     When is yoda's birthday? 
+//     What planet is yoda from? 
+//     Which of the following characters is not a part of the Star Wars universe? 
+//     What films does yoga appear in? Or does not appear in? 
+// -From films api:
+//     What is the title of episode 4?
+//     Who directed episode 5?
+//     Who produced episode 5? 
+//     What date was episode 5 released on? 
+//     How does the opening crawl of episode 5 begin? 
+//     The opening crawl of episode ___ begins with "blah blah blah"
+// -From species api:
+//     This planet is homeworld to this species
+//     What is the native language of this species? 
+// -From vehicles api:
+//     Which of the following is not a vehicle from the star wars universe? 
+//     Who is the manufacturer of this vehicle? 
+//     What films does this vehicle appear in? 
+// -From starship api:
+//     What films does this starship appear in? 
+//     Who is the manufacturer of this starship? 
+//     What starship has character X driven (or not driven)? 
+
+
+// */
