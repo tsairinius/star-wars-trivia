8e6c9c45ca809705cd594ac6f27a0c1ead1aefc8
Eric T <9063787+tsairinius@users.noreply.github.com>
2021-01-31 13:39:52 -0800

    Created separate webpack config files for development and production. Also set up development server using webpack

--- webpack.common.cjs
+++ webpack.common.cjs
@@ -1,13 +1,7 @@
-const path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
   entry: './src/index.js',
-  output: {
-    filename: 'main.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
       {
